7f577ccb329171c404ab3c86999838dd755cb701
Kirill Illenseer <Zam.K@live.de>
2023-09-09 09:07:57 +0200

    Downsized overengineering

--- Desmond.Killing Floor 2/Crutch.cs
+++ Desmond.Killing Floor 2/Crutch.cs
@@ -26,23 +26,24 @@ internal class Crutch
 
     static string GetHTML(IEnumerable<KF2Server> Farm, IPAddress Address)
     {
-        static string Title(IEnumerable<KF2Server> Farm) => $"<title>{string.Join(" | ", Farm.Select(_ => _.ServerName!).Distinct())}</title>";
+        var Title = $"<title>{string.Join(" | ", Farm.Select(_ => _.ServerName!).Distinct())}</title>";
 
         var Links = "<link rel=\"shortcut icon\" href=\"favicon.ico\" type=\"image/x-icon\"><link rel=\"stylesheet\" type=\"text/css\" href=\"kf2.css\"><link rel=\"stylesheet\" type=\"text/css\" href=\"kf2modern.css\">";
 
-        static string Script(IEnumerable<KF2Server> Farm) => $"<script type=\"text/javascript\">function WebAdmin(Port){{window.location.replace(window.location.protocol +\"//\"+window.location.hostname+\":\"+Port)}}</script>";
+        var Script = $"<script type=\"text/javascript\">function WebAdmin(Port){{window.location.replace(window.location.protocol +\"//\"+window.location.hostname+\":\"+Port)}}</script>";
 
-        static string Table(IEnumerable<KF2Server> Farm, IPAddress Address) => "<table><tr>" +
+        var Table = "<table><tr>" +
             string.Join("</tr><tr>", Farm.Select(Server =>
                 $"<td><a href=\"steam://rungameid/232090//-SteamConnectIP={Address}:{Server.Port}\">{Server.ConfigSubDir}</a></td>" +
                 //$"<td><a href=\"steam://connect/{Address}:{Server.Port}\">&#x267F;</a></td>" +//TODO: connect in-game somehow
                 $"{"<td>"}{(Server.AdminPort is not null ?
                     $"<a href=# onclick=\"WebAdmin(" + Server.AdminPort + ")\">&#x1F9D9</a>" :
-                    "&#x274C")}{"</td>"}"
+                    "&#x274C")}"+
+                    "</td>"
         )) + "</tr></table>";
 
         var Footer = "<footer>" + DateTime.Now.ToString("o") + "</footer>";
 
-        return $"<!doctype html><head>{Title(Farm)}{Links}{Script(Farm)}</head><body>{Table(Farm, Address)}{Footer}</body></html>";
+        return $"<!doctype html><head>{Title}{Links}{Script}</head><body>{Table}{Footer}</body></html>";
     }
 }
